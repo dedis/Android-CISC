e4aed30af0dd93c215d15f48a9e59cb354eab5fe
qant <andrea.caforio@epfl.ch>
2016-07-14 17:36:16 +0200

    Introduce Ed25519 key pair generation
    Change:   Add comments to .gitignore
    Feature:  The str4d/ed25519 java-port library allows for generating
              public and private keys using the twisted Edwards curve.
    Bugfix:   None.

---
+++ app/src/main/java/com/epfl/dedis/crypto/Identity.java
@@ -0,0 +1,53 @@
+package com.epfl.dedis.crypto;
+
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Arrays;
+
+import net.i2p.crypto.eddsa.KeyPairGenerator;
+
+/**
+ * The Identity class generates and contains the public and private
+ * Ed25519-keys of a device. Used for Cothority authentication.
+ *
+ * @author Ignacio Aléman
+ * @author Andrea Caforio
+ * @since 14.07.2016
+ */
+public class Identity {
+
+    /* Ed25519 key pair */
+    private PublicKey publicKey;
+    private PrivateKey privateKey;
+
+    public Identity() {
+        KeyPair keyPair = new KeyPairGenerator().generateKeyPair();
+        this.publicKey = keyPair.getPublic();
+        this.privateKey = keyPair.getPrivate();
+    }
+
+    ////////////////////////////////////////////
+    // TODO actual key-storage and access
+    ////////////////////////////////////////////
+    public PublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public PrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+    /**
+     * Print neatly (byte array uses for keys).
+     *
+     * @return identity string
+     */
+    @Override
+    public String toString() {
+        return "Identity {\n" +
+                "\tpublicKey=" + Arrays.toString(publicKey.getEncoded()) + ",\n" +
+                "\tprivateKey=" + Arrays.toString(privateKey.getEncoded()) + "\n" +
+                '}';
+    }
+}
