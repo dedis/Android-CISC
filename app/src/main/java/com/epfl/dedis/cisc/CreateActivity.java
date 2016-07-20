1835df0fcdb76c5c2c4c95aa8a017d7225ddede0
Andrea Caforio <andrea.caforio@epfl.ch>
2016-07-20 14:19:43 +0200

    Expand CreateActivity
    Change:   Added assertions when referencing UI-objects.
    Feature:  Clear button to empty EditText fields.
    Bugfix:   None.

--- app/src/main/java/com/epfl/dedis/cisc/CreateActivity.java
+++ app/src/main/java/com/epfl/dedis/cisc/CreateActivity.java
@@ -1,13 +1,93 @@
 package com.epfl.dedis.cisc;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CreateActivity extends AppCompatActivity {
+
+    private EditText mHostEditText;
+    private EditText mPortEditText;
+    private EditText mDataEditText;
+
+    private InputMethodManager imm;
+
+    private void clear() {
+        mHostEditText.setText("");
+        mPortEditText.setText("");
+        mDataEditText.setText("");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        mHostEditText = (EditText) findViewById(R.id.host_editText);
+        assert mHostEditText != null;
+        mHostEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE) {
+                    mHostEditText.clearFocus();
+                    imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(mHostEditText.getWindowToken(), 0);
+                }
+                return false;
+            }
+        });
+
+        mPortEditText = (EditText) findViewById(R.id.port_editText);
+        assert mPortEditText != null;
+        mPortEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE) {
+                    mPortEditText.clearFocus();
+                    imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(mPortEditText.getWindowToken(), 0);
+                }
+                return false;
+            }
+        });
+
+        mDataEditText = (EditText) findViewById(R.id.data_editText);
+        assert mPortEditText != null;
+        mDataEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE) {
+                    mDataEditText.clearFocus();
+                    imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(mDataEditText.getWindowToken(), 0);
+                }
+                return false;
+            }
+        });
+
+        Button mClearButton = (Button) findViewById(R.id.clear_button);
+        assert mClearButton != null;
+        mClearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               clear();
+            }
+        });
+
+        Button mCreateButton = (Button) findViewById(R.id.create_button);
+        assert mCreateButton != null;
+        mCreateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("START NEW SKIPCHAIN!");
+            }
+        });
     }
 }
--- app/src/main/java/com/epfl/dedis/cisc/MainActivity.java
+++ app/src/main/java/com/epfl/dedis/cisc/MainActivity.java
@@ -1,56 +1,68 @@
 package com.epfl.dedis.cisc;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import com.epfl.dedis.net.Connection;
-
-/**
- * The MainActivity class defines the UI of the
- * start screen and manages to connection to the
- * Cothority server.
- *
- * @author Ignacio Aléman
- * @author Andrea Caforio
- * @since 08.07.2016
- */
 public class MainActivity extends AppCompatActivity {
 
-    private SharedPreferences preferences;                  /* Key-value storage */
-    private static final String HOST_KEY = "hostname";      /* Hostname key */
-    private static final String PORT_KEY = "portnumber";    /* Portnumber key */
+    private TextView mIdentityValue;
+    private TextView mStatusValue;
+    private TextView mPollValue;
 
-    /**
-     * Display common toast marker on the screen.
-     *
-     * @param text resource id of the string to be displayed
-     */
-    public void toast(int text) {
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    private static final String HISTORY = "HISTORY";
+    private static final String HOST_KEY = "HOST";
+    private static final String PORT_KEY = "PORT";
+    private static final String ID_KEY = "ID";
+
+    private boolean checkHistory() {
+        SharedPreferences pref = getSharedPreferences(HISTORY, Context.MODE_PRIVATE);
+        String host = pref.getString(HOST_KEY, "");
+        String port = pref.getString(PORT_KEY, "");
+        String id = pref.getString(ID_KEY, "");
+
+        if (host.isEmpty() || port.isEmpty() || id.isEmpty()) {
+            mIdentityValue.setText("n/a");
+            mStatusValue.setText("n/a");
+            mPollValue.setText("n/a");
+            return false;
+        }
+        return true;
     }
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button mCreateButton = (Button) findViewById(R.id.create_button);
+        mIdentityValue = (TextView) findViewById(R.id.id_value);
+        assert mIdentityValue != null;
+
+        mStatusValue = (TextView) findViewById(R.id.status_value);
+        assert mStatusValue != null;
+
+        mPollValue = (TextView) findViewById(R.id.poll_value);
+        assert mPollValue != null;
+
+        if (checkHistory()) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        Button mCreateButton = (Button) findViewById(R.id.new_button);
         assert mCreateButton != null;
         mCreateButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, CreateActivity.class);
                 startActivity(i);
             }
         });
-
     }
 }
