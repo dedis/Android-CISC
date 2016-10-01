c728ef9eaac8761d0a720f6c507ddaedb251f120
Andrea Caforio <andrea.caforio@epfl.ch>
2016-10-01 23:18:17 +0200

    Remove ExpandableListView
    Change:   Replace ExpandableListView with a simple ListView.
    Feature:  None.
    Bugfix:   None.

--- app/src/main/java/com/epfl/dedis/cisc/DeviceActivity.java
+++ app/src/main/java/com/epfl/dedis/cisc/DeviceActivity.java
@@ -2,15 +2,10 @@ package com.epfl.dedis.cisc;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseExpandableListAdapter;
-import android.widget.ExpandableListView;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.epfl.dedis.crypto.Utils;
 import com.epfl.dedis.net.Config;
@@ -23,128 +18,33 @@ import java.util.Map;
 
 public class DeviceActivity extends AppCompatActivity implements Activity {
 
+    private static final String[] FROM = new String[]{"name", "data"};
+    private static final int[] TO = new int[]{android.R.id.text1, android.R.id.text2};
+
     public void taskJoin() {}
     public void taskFail(int error) {}
-
-    // TODO: Find nicer way for expandable list?
-    private class ExpandableListAdapter extends BaseExpandableListAdapter {
-
-        private Context context;
-        private List<String> expandableListTitle;
-        private Map<String, List<String>> expandableListDetail;
-
-        public ExpandableListAdapter(Context context, List<String> expandableListTitle,
-                                     Map<String, List<String>> expandableListDetail) {
-            this.context = context;
-            this.expandableListTitle = expandableListTitle;
-            this.expandableListDetail = expandableListDetail;
-        }
-
-        @Override
-        public Object getChild(int listPosition, int expandedListPosition) {
-            return this.expandableListDetail.get(this.expandableListTitle.get(listPosition))
-                    .get(expandedListPosition);
-        }
-
-        @Override
-        public long getChildId(int listPosition, int expandedListPosition) {
-            return expandedListPosition;
-        }
-
-        @Override
-        public View getChildView(int listPosition, final int expandedListPosition,
-                                 boolean isLastChild, View convertView, ViewGroup parent) {
-            final String expandedListText = (String) getChild(listPosition, expandedListPosition);
-            if (convertView == null) {
-                LayoutInflater layoutInflater = (LayoutInflater) this.context
-                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                convertView = layoutInflater.inflate(R.layout.list_item, parent, false);
-            }
-            TextView expandedListTextView = (TextView) convertView
-                    .findViewById(R.id.expandedListItem);
-            expandedListTextView.setText(expandedListText);
-            return convertView;
-        }
-
-        @Override
-        public int getChildrenCount(int listPosition) {
-            return this.expandableListDetail.get(this.expandableListTitle.get(listPosition))
-                    .size();
-        }
-
-        @Override
-        public Object getGroup(int listPosition) {
-            return this.expandableListTitle.get(listPosition);
-        }
-
-        @Override
-        public int getGroupCount() {
-            return this.expandableListTitle.size();
-        }
-
-        @Override
-        public long getGroupId(int listPosition) {
-            return listPosition;
-        }
-
-        @Override
-        public View getGroupView(int listPosition, boolean isExpanded,
-                                 View convertView, ViewGroup parent) {
-            String listTitle = (String) getGroup(listPosition);
-            if (convertView == null) {
-                LayoutInflater layoutInflater = (LayoutInflater) this.context.
-                        getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                convertView = layoutInflater.inflate(R.layout.list_group, parent, false);
-            }
-            TextView listTitleTextView = (TextView) convertView
-                    .findViewById(R.id.listTitle);
-            listTitleTextView.setTypeface(null, Typeface.BOLD);
-            listTitleTextView.setText(listTitle);
-            return convertView;
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return false;
-        }
-
-        @Override
-        public boolean isChildSelectable(int listPosition, int expandedListPosition) {
-            return true;
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device);
 
-        ExpandableListView expandableListView = (ExpandableListView) findViewById(R.id.device_device_list);
+        ListView listView = (ListView) findViewById(R.id.device_device_list);
 
         SharedPreferences sharedPreferences = getSharedPreferences(PREF, Context.MODE_PRIVATE);
         Config config = Utils.fromJson(sharedPreferences.getString(IDENTITY, ""), Identity.class).getConfig();
 
         Map<String, String> devices = config.getDevice();
-        Map<String, String> data = config.getData();
-
-        List<String> headers = new ArrayList<>(devices.keySet());
-        Map<String, List<String>> children = new HashMap<>();
+        List<Map<String, String>> data = new ArrayList<>();
 
         for (Map.Entry<String, String> entry : devices.entrySet()) {
-            List<String> singleton = new ArrayList<>();
-            singleton.add(entry.getValue());
-            children.put(entry.getKey(), singleton);
+            Map<String, String> datum = new HashMap<>(2);
+            datum.put("name", entry.getKey());
+            datum.put("data", entry.getValue());
+            data.add(datum);
         }
 
-        // TODO: Cothority doesn't create data entry with equal key
-        for (Map.Entry<String, String> entry : data.entrySet()) {
-            for (Map.Entry<String, String> e : devices.entrySet()) {
-                if (entry.getKey().contains(e.getKey())) {
-                    children.get(e.getKey()).add(entry.getValue());
-                }
-            }
-        }
-        ExpandableListAdapter ela = new ExpandableListAdapter(this, headers, children);
-        expandableListView.setAdapter(ela);
+        SimpleAdapter adapter = new SimpleAdapter(this, data, android.R.layout.simple_list_item_2, FROM, TO);
+        listView.setAdapter(adapter);
     }
 }
